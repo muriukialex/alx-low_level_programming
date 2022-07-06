11a44d3e91f9f2d32e6e26a655fd99452097734e
muriukialex <alexmuriukimaina254@gmail.com>
2022-07-06 12:24:31 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,15 +10,14 @@
 
 int main(void)
 {
-		int n = 0;
-			char str_hbt[8] = "_putchar";
+	int n = 0;
+	char str_hbt[8] = "_putchar";
 
-				while (n < 8)
-						{
-									_putchar(str_hbt[n]);
-											n += 1;
-												}
-					_putchar('\n');
-						return (0);
+	while (n < 8){
+		_putchar(str_hbt[n]);
+		n += 1;
+	}
+	_putchar('\n');
+	return (0);
 }
 
